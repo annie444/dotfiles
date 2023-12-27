066160b78829f34f911cd20827a0556060d72e17
Annie Ehler <annie.ehler.4@gmail.com>
2023-12-26 20:23:39 -0800

    Add .finder-defaults.fish Update .config/nvim/lua/plugins/lsp/init.lua

---
+++ .finder-defaults.fish
@@ -0,0 +1,55 @@
+defaults write "com.apple.finder" "AppleShowAllExtensions" -boolean true
+defaults write "com.apple.finder" "AppleShowAllFiles" -boolean true
+defaults write "com.apple.finder" "CopyProgressWindowLocation" -string "{-integer 1304, 404}"
+defaults write "com.apple.finder" "CreateDesktop" -boolean true
+defaults write "com.apple.finder" "DesktopViewSettings.IconViewSettings.arrangeBy" none
+defaults write "com.apple.finder" "DesktopViewSettings.IconViewSettings.gridOffsetX" -boolean false
+defaults write "com.apple.finder" "DesktopViewSettings.IconViewSettings.gridOffsetY" -boolean false
+defaults write "com.apple.finder" "DesktopViewSettings.IconViewSettings.gridSpacing" -integer 54
+defaults write "com.apple.finder" "DesktopViewSettings.IconViewSettings.iconSize" -integer 64
+defaults write "com.apple.finder" "DesktopViewSettings.IconViewSettings.labelOnBottom" -boolean true
+defaults write "com.apple.finder" "DesktopViewSettings.IconViewSettings.showIconPreview" -boolean true
+defaults write "com.apple.finder" "DesktopViewSettings.IconViewSettings.showItemInfo" -boolean true
+defaults write "com.apple.finder" "DesktopViewSettings.IconViewSettings.textSize" -integer 12
+defaults write "com.apple.finder" "DownloadsFolderListViewSettingsVersion" -boolean true
+defaults write "com.apple.finder" "FK_AppCentricShowSidebar" -boolean true
+defaults write "com.apple.finder" "FK_ArrangeBy" -string "Date Added"
+defaults write "com.apple.finder" "FK_StandardViewSettings.ExtendedListViewSettingsV2.calculateAllSizes" -boolean true
+defaults write "com.apple.finder" "FK_StandardViewSettings.ExtendedListViewSettingsV2.iconSize" -integer 16
+defaults write "com.apple.finder" "FK_StandardViewSettings.ExtendedListViewSettingsV2.showIconPreview" -boolean true
+defaults write "com.apple.finder" "FK_StandardViewSettings.ExtendedListViewSettingsV2.sortColumn" -string name
+defaults write "com.apple.finder" "FK_StandardViewSettings.ExtendedListViewSettingsV2.textSize" -integer 13
+defaults write "com.apple.finder" "FK_StandardViewSettings.ExtendedListViewSettingsV2.useRelativeDates" -boolean true
+defaults write "com.apple.finder" "FK_StandardViewSettings.ExtendedListViewSettingsV2.viewOptionsVersion" -boolean true
+defaults write "com.apple.finder" "FK_StandardViewSettings.IconViewSettings.arrangeBy" none
+defaults write "com.apple.finder" "FK_StandardViewSettings.IconViewSettings.gridOffsetX" -boolean false
+defaults write "com.apple.finder" "FK_StandardViewSettings.IconViewSettings.gridOffsetY" -boolean false
+defaults write "com.apple.finder" "FK_StandardViewSettings.IconViewSettings.gridSpacing" -integer 54
+defaults write "com.apple.finder" "FK_StandardViewSettings.IconViewSettings.iconSize" -integer 64
+defaults write "com.apple.finder" "FK_StandardViewSettings.IconViewSettings.labelOnBottom" -boolean true
+defaults write "com.apple.finder" "FK_StandardViewSettings.IconViewSettings.showIconPreview" -boolean true
+defaults write "com.apple.finder" "FK_StandardViewSettings.IconViewSettings.showItemInfo" -boolean true
+defaults write "com.apple.finder" "FK_StandardViewSettings.IconViewSettings.textSize" -integer 12
+defaults write "com.apple.finder" "FXArrangeGroupViewBy" -string "Date Added"
+defaults write "com.apple.finder" "FXEnableExtensionChangeWarning" -boolean true
+defaults write "com.apple.finder" "FXICloudDriveDesktop" -boolean true
+defaults write "com.apple.finder" "FXICloudDriveDocuments" -boolean true
+defaults write "com.apple.finder" "FXICloudDriveEnabled" -boolean true
+defaults write "com.apple.finder" "FXICloudDriveFirstSyncDownComplete" -boolean true
+defaults write "com.apple.finder" "FXICloudLoggedIn" -boolean true
+defaults write "com.apple.finder" "FXPreferredGroupBy" -string "Date Added"
+defaults write "com.apple.finder" "LastTrashState" -boolean true
+defaults write "com.apple.finder" "RecentsArrangeGroupViewBy" -string "Date Last Opened"
+defaults write "com.apple.finder" "ShowExternalHardDrivesOnDesktop" -boolean true
+defaults write "com.apple.finder" "ShowHardDrivesOnDesktop" -boolean true
+defaults write "com.apple.finder" "ShowMountedServersOnDesktop" -boolean true
+defaults write "com.apple.finder" "ShowPathbar" -boolean true
+defaults write "com.apple.finder" "ShowRemovableMediaOnDesktop" -boolean true
+defaults write "com.apple.finder" "ShowSidebar" -boolean true
+defaults write "com.apple.finder" "ShowStatusBar" -boolean true
+defaults write "com.apple.finder" "SidebarDevicesSectionDisclosedState" -boolean true
+defaults write "com.apple.finder" "SidebarPlacesSectionDisclosedState" -boolean true
+defaults write "com.apple.finder" "SidebarShowingSignedIntoiCloud" -boolean true
+defaults write "com.apple.finder" "SidebarShowingiCloudDesktop" -boolean true
+defaults write "com.apple.finder" "SidebarWidth" -integer 154
+defaults write "com.apple.finder" "TagsCloudSerialNumber" -integer 2
