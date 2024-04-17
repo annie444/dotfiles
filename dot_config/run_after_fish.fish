337c19fa02e83caf3974dbdd1325ed40a1fbf0ad
Annie Ehler <annie.ehler.4@gmail.com>
2024-04-17 11:12:52 -0700

    Have chezmoi handle fisher

---
+++ dot_config/run_after_fish.fish
@@ -0,0 +1,16 @@
+#!/usr/bin/env fish
+
+curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source 
+fisher install jorgebucaran/fisher
+fisher install patrickf1/fzf.fish
+fisher install franciscolourenco/done
+fisher install jorgebucaran/spark.fish
+fisher install joseluisq/gitnow@latest
+fisher install meaningful-ooo/sponge
+fisher install jorgebucaran/autopair.fish
+fisher install jorgebucaran/getopts.fish
+fisher install nickeb96/puffer-fish
+fisher install z11i/github-copilot-cli.fish
+fisher install gazorby/fish-abbreviation-tips
+fisher install fabioantunes/base16-fish-shell
+fisher install dracula/fish
