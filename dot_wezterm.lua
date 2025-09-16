39403929868a1f8d54a580b244fa155da5d7c843
Annie Ehler <annie.ehler.4@gmail.com>
2025-09-16 15:11:29 -0700

    Update dot_wezterm.lua

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -58,8 +58,8 @@ config.colors = {
 }
 
 config.enable_tab_bar = true
-config.hide_tab_bar_if_only_one_tab = true
-config.tab_max_width = 32
+config.hide_tab_bar_if_only_one_tab = false
+config.tab_max_width = 16
 
 config.window_padding = {
 	left = "1cell",
@@ -142,15 +142,15 @@ end
 
 config.leader = { key = " ", mods = "CTRL", timeout_milliseconds = 1000 }
 config.keys = {
-	{ key = "F11", mods = "", action = wezterm.action.ToggleFullScreen },
-	{ key = "h", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Left") },
-	{ key = "l", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Right") },
-	{ key = "k", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Up") },
-	{ key = "j", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Down") },
-	{ key = "H", mods = "LEADER", action = wezterm.action.AdjustPaneSize({ "Left", 5 }) },
-	{ key = "L", mods = "LEADER", action = wezterm.action.AdjustPaneSize({ "Right", 5 }) },
-	{ key = "K", mods = "LEADER", action = wezterm.action.AdjustPaneSize({ "Up", 5 }) },
-	{ key = "J", mods = "LEADER", action = wezterm.action.AdjustPaneSize({ "Down", 5 }) },
+	{ key = "F11", mods = "",       action = wezterm.action.ToggleFullScreen },
+	{ key = "h",   mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Left") },
+	{ key = "l",   mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Right") },
+	{ key = "k",   mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Up") },
+	{ key = "j",   mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Down") },
+	{ key = "H",   mods = "LEADER", action = wezterm.action.AdjustPaneSize({ "Left", 5 }) },
+	{ key = "L",   mods = "LEADER", action = wezterm.action.AdjustPaneSize({ "Right", 5 }) },
+	{ key = "K",   mods = "LEADER", action = wezterm.action.AdjustPaneSize({ "Up", 5 }) },
+	{ key = "J",   mods = "LEADER", action = wezterm.action.AdjustPaneSize({ "Down", 5 }) },
 	{
 		key = "-",
 		mods = "LEADER",
@@ -161,29 +161,29 @@ config.keys = {
 		mods = "LEADER",
 		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
 	},
-	{ key = ":", mods = "LEADER", action = wezterm.action.ActivateCommandPalette },
-	{ key = "q", mods = "LEADER", action = wezterm.action.CloseCurrentPane({ confirm = false }) },
-	{ key = "d", mods = "LEADER", action = wezterm.action.DetachDomain("CurrentPaneDomain") },
-	{ key = "v", mods = "LEADER", action = wezterm.action.ActivateCopyMode },
-	{ key = "x", mods = "LEADER", action = wezterm.action.ActivateCopyMode },
-	{ key = "f", mods = "LEADER", action = wezterm.action.ToggleFullScreen },
-	{ key = ">", mods = "CTRL|SHIFT", action = wezterm.action.SpawnTab("CurrentPaneDomain") },
-	{ key = "<", mods = "CTRL|SHIFT", action = wezterm.action.SpawnTab("CurrentPaneDomain") },
-	{ key = "L", mods = "CTRL|SHIFT", action = wezterm.action.ActivateTabRelative(1) },
-	{ key = "H", mods = "CTRL|SHIFT", action = wezterm.action.ActivateTabRelative(-1) },
-	{ key = "K", mods = "CTRL|SHIFT", action = wezterm.action.MoveTabRelative(1) },
-	{ key = "J", mods = "CTRL|SHIFT", action = wezterm.action.MoveTabRelative(-1) },
-	{ key = "Q", mods = "CTRL|SHIFT", action = wezterm.action.CloseCurrentTab({ confirm = false }) },
-	{ key = "C", mods = "CTRL|SHIFT", action = wezterm.action.CopyTo("Clipboard") },
-	{ key = "V", mods = "CTRL|SHIFT", action = wezterm.action.PasteFrom("Clipboard") },
-	{ key = "X", mods = "CTRL|SHIFT", action = wezterm.action.ActivateCopyMode },
-	{ key = "F", mods = "CTRL|SHIFT", action = wezterm.action.ToggleFullScreen },
-	{ key = "l", mods = "CTRL", action = wezterm.action.ShowDebugOverlay },
-	{ key = "-", mods = "CTRL", action = wezterm.action.DecreaseFontSize },
-	{ key = "=", mods = "CTRL", action = wezterm.action.IncreaseFontSize },
-	{ key = "0", mods = "CTRL", action = wezterm.action.ResetFontSize },
-	{ key = "PageUp", mods = "SHIFT", action = wezterm.action.ScrollByPage(-1) },
-	{ key = "PageDown", mods = "SHIFT", action = wezterm.action.ScrollByPage(1) },
+	{ key = ":",        mods = "LEADER",     action = wezterm.action.ActivateCommandPalette },
+	{ key = "q",        mods = "LEADER",     action = wezterm.action.CloseCurrentPane({ confirm = false }) },
+	{ key = "d",        mods = "LEADER",     action = wezterm.action.DetachDomain("CurrentPaneDomain") },
+	{ key = "v",        mods = "LEADER",     action = wezterm.action.ActivateCopyMode },
+	{ key = "x",        mods = "LEADER",     action = wezterm.action.ActivateCopyMode },
+	{ key = "f",        mods = "LEADER",     action = wezterm.action.ToggleFullScreen },
+	{ key = ">",        mods = "CTRL|SHIFT", action = wezterm.action.SpawnTab("CurrentPaneDomain") },
+	{ key = "<",        mods = "CTRL|SHIFT", action = wezterm.action.SpawnTab("CurrentPaneDomain") },
+	{ key = "L",        mods = "CTRL|SHIFT", action = wezterm.action.ActivateTabRelative(1) },
+	{ key = "H",        mods = "CTRL|SHIFT", action = wezterm.action.ActivateTabRelative(-1) },
+	{ key = "K",        mods = "CTRL|SHIFT", action = wezterm.action.MoveTabRelative(1) },
+	{ key = "J",        mods = "CTRL|SHIFT", action = wezterm.action.MoveTabRelative(-1) },
+	{ key = "Q",        mods = "CTRL|SHIFT", action = wezterm.action.CloseCurrentTab({ confirm = false }) },
+	{ key = "C",        mods = "CTRL|SHIFT", action = wezterm.action.CopyTo("Clipboard") },
+	{ key = "V",        mods = "CTRL|SHIFT", action = wezterm.action.PasteFrom("Clipboard") },
+	{ key = "X",        mods = "CTRL|SHIFT", action = wezterm.action.ActivateCopyMode },
+	{ key = "F",        mods = "CTRL|SHIFT", action = wezterm.action.ToggleFullScreen },
+	{ key = "l",        mods = "CTRL",       action = wezterm.action.ShowDebugOverlay },
+	{ key = "-",        mods = "CTRL",       action = wezterm.action.DecreaseFontSize },
+	{ key = "=",        mods = "CTRL",       action = wezterm.action.IncreaseFontSize },
+	{ key = "0",        mods = "CTRL",       action = wezterm.action.ResetFontSize },
+	{ key = "PageUp",   mods = "SHIFT",      action = wezterm.action.ScrollByPage(-1) },
+	{ key = "PageDown", mods = "SHIFT",      action = wezterm.action.ScrollByPage(1) },
 }
 
 -- ClearSelection
@@ -273,7 +273,7 @@ tabline.setup({
 			"index",
 			{ "parent", padding = 0 },
 			"/",
-			{ "cwd", padding = { left = 0, right = 1 } },
+			{ "cwd",    padding = { left = 0, right = 1 } },
 			{ "zoomed", padding = 0 },
 		},
 		tab_inactive = { "index", { "process", padding = { left = 0, right = 1 } } },
