2e67292f7c44b87f1ce5b2bccbf38171d6b3b54f
Annie Ehler <annie.ehler.4@gmail.com>
2025-09-19 12:36:23 -0700

    Update .gitmodules Add dot_config/wezterm Add private_dot_ssh/encrypted_github_idecdsa.pub.age Remove private_dot_ssh/github_idecdsa.pub Update run_after_private_dot_ssh.sh Change attributes of private_dot_ssh/encrypted_private_executable_config.tmpl.age Change attributes of private_dot_ssh/encrypted_private_hetzner_id_ed25519.age

--- run_after_private_dot_ssh.sh
+++ run_after_private_dot_ssh.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-chmod -R 700 ~/.ssh
-chmod -R 700 ~/.ssh/*
-chmod -R 600 ~/.ssh/*ed25519*
-chmod -R 600 ~/.ssh/*id*
-chmod -R 644 ~/.ssh/*.pub
+# chmod -R 700 ~/.ssh
+# chmod -R 700 ~/.ssh/*
+# chmod -R 600 ~/.ssh/*ed25519*
+# chmod -R 600 ~/.ssh/*id*
+# chmod -R 644 ~/.ssh/*.pub
