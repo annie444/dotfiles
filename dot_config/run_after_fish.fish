2940f1c9bce0ccf310ed7f46537ef884b8df2247
Annie Ehler <annie.ehler.4@gmail.com>
2025-12-16 13:17:40 -0800

    Fisher will automatically install plugins in the fish_plugins file

--- dot_config/run_after_fish.fish
+++ dot_config/run_after_fish.fish
@@ -1,24 +1,24 @@
 #!/usr/bin/env fish
 
-function install_plugin
-  if test -z (grep -i "$argv[1]" ~/.config/fish/fish_plugins)
-    fisher install $argv[1]
-  else
-    fisher update $argv[1]
-  end
+function install_fish_plugin -a package -a file
+    if test -f $file
+        fisher install $package
+    else
+        fisher update $package
+    end
 end
 
 if ! test -f ~/.config/fish/functions/fisher.fish
-  curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source
+    curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source
 end
-install_plugin jorgebucaran/fisher
-install_plugin PatrickF1/fzf.fish
-install_plugin franciscolourenco/done
-install_plugin jorgebucaran/spark.fish
-install_plugin jorgebucaran/autopair.fish
-install_plugin jorgebucaran/getopts.fish
-install_plugin nickeb96/puffer-fish
-install_plugin z11i/github-copilot-cli.fish
-install_plugin gazorby/fish-abbreviation-tips
-install_plugin jorgebucaran/nvm.fish
-install_plugin dracula/fish
+
+if ! test -f ~/.config/fish/fish_plugins
+    set_color red
+    echo "fish_plugins file not found! Creating an empty one."
+    set_color normal
+    touch ~/.config/fish/fish_plugins
+end
+
+install_fish_plugin jorgebucaran/fisher ~/.config/fish/functions/fisher.fish
+
+fisher update
